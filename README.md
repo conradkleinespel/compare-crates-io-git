faec8b07418f4ac453bddb232dc63852642e7361
Conrad Kleinespel <conradk@conradk.com>
2024-04-14 21:41:47 +0200

    Check for sha1 announced in crates.io within commit history of default branch

--- README.md
+++ README.md
@@ -10,7 +10,29 @@ This projects aims to detect differences between what's published on Crates.io v
 ```shell
 cargo run rpassword 7.1.0
 
-# Downloading rpassword/7.1.0 to /tmp/.tmp1DSId2
-# Repository is https://github.com/conradkleinespel/rpassword.git, subpath is ''
-# Cloned repository to /tmp/.tmpfSg1wt
+#Downloading rpassword/7.1.0 to /tmp/.tmpA0p9Gm
+#Repository is https://github.com/conradkleinespel/rpassword.git, subpath is ''
+#Cloned repository to /tmp/.tmpT0CO8T
+#Default branch is master
+#Sha1 announced in crates.io is 77da0606017f26e476c51d2051c6042db9c1fe4f
+#Sha1 commit was 77da0606017f26e476c51d2051c6042db9c1fe4f (20/10/2022 16:06): Handle Ctrl-U in rpassword
+#Sha1 commit was 77da0606017f26e476c51d2051c6042db9c1fe4f (20/10/2022 16:06): Handle Ctrl-U in rpassword
+
+cargo run linux-raw-sys 0.4.13
+
+#Downloading linux-raw-sys/0.4.13 to /tmp/.tmpNBXXWM
+#Repository is https://github.com/sunfishcode/linux-raw-sys.git, subpath is ''
+#Cloned repository to /tmp/.tmphG83ky
+#Default branch is main
+#Sha1 announced in crates.io is ad726d4998270502f292e1ab9a580217878b674a
+#Commit not in default branch history (using revwalk)
+#Commit not in default branch history (using descendants)
+
+cargo run web-sys 0.3.67
+
+#Downloading web-sys/0.3.67 to /tmp/.tmpbLj3NV
+#Repository is https://github.com/rustwasm/wasm-bindgen.git, subpath is 'crates/web-sys'
+#Cloned repository to /tmp/.tmpiYvCtH
+#Default branch is main
+#No sha1 announced in crates.io, crate packaged with --allow-dirty
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use flate2::read::GzDecoder;
+use git2::{Oid, Reference, Repository, Sort};
+use serde::Deserialize;
 use std::env::args;
 use std::fs::File;
 use std::io::{Cursor, Read};
 use std::path::Path;
-use flate2::read::GzDecoder;
-use git2::Repository;
-use serde::Deserialize;
 use tar::Archive;
 use url::Url;
 
@@ -18,6 +18,16 @@ struct Package {
     repository: String,
 }
 
+#[derive(Deserialize)]
+struct CargoVcsInfoJson {
+    git: CargoVcsInfoJsonGit,
+}
+
+#[derive(Deserialize)]
+struct CargoVcsInfoJsonGit {
+    sha1: String,
+}
+
 fn main() {
     let argv: Vec<String> = args().collect();
 
@@ -28,31 +38,135 @@ fn main() {
     let crates_io_tempdir = tempfile::tempdir().unwrap();
     download_crate(crate_name, crate_version, crates_io_tempdir.path());
 
-    let crates_io_path = crates_io_tempdir.into_path()
+    let crates_io_path = crates_io_tempdir
+        .into_path()
         .join(format!("{}-{}", crate_name, crate_version));
 
-    let (repository_url, subpath) = get_repository_and_subpath_from_repository_url(
-        crates_io_path.as_path()
-    );
+    let (repository_url, subpath) =
+        get_repository_and_subpath_from_repository_url(crates_io_path.as_path());
     println!("Repository is {}, subpath is '{}'", repository_url, subpath);
 
     let repository = match Repository::clone(repository_url.as_str(), git_tempdir.path()) {
         Err(_) => {
             println!("Invalid repository");
-            return
+            return;
         }
-        Ok(repository) => {repository}
+        Ok(repository) => repository,
     };
+    println!(
+        "Cloned repository to {}",
+        repository.path().parent().unwrap().to_str().unwrap()
+    );
+    let head = repository.head().unwrap();
+    println!("Default branch is {}", head.shorthand().unwrap());
 
-    println!("Cloned repository to {}", repository.path().parent().unwrap().to_str().unwrap());
+    match get_expected_sha1_from_crate(crates_io_path.as_path()) {
+        Some(sha1) => {
+            println!("Sha1 announced in crates.io is {}", sha1);
+            if !sha1_in_commit_history_on_default_branch_with_revwalk(&repository, &head, sha1.as_str()) {
+                println!("Commit not in default branch history (using revwalk)")
+            }
+            if !sha1_in_commit_history_on_default_branch_with_descendants(&repository, &head, sha1.as_str()) {
+                println!("Commit not in default branch history (using descendants)")
+            }
+        }
+        None => {
+            println!("No sha1 announced in crates.io, crate packaged with --allow-dirty");
+        }
+    }
+}
+
+fn sha1_in_commit_history_on_default_branch_with_revwalk(
+    repository: &Repository,
+    head: &Reference,
+    sha1: &str,
+) -> bool {
+    let mut revwalk = repository.revwalk().unwrap();
+    revwalk.push(head.target().unwrap()).unwrap();
+    revwalk.set_sorting(Sort::TOPOLOGICAL | Sort::TIME).unwrap();
+    loop {
+        let current = match revwalk.next() {
+            None => {
+                break;
+            }
+            Some(current) => current,
+        };
+
+        let current = match current {
+            Err(err) => {
+                println!("Revwalk error {:?}", err);
+                break;
+            }
+            Ok(current) => current,
+        };
+
+        if current.to_string() == sha1 {
+            find_and_print_commit(repository, current);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn sha1_in_commit_history_on_default_branch_with_descendants(
+    repository: &Repository,
+    head: &Reference,
+    sha1: &str,
+) -> bool {
+    let sha1_oid = Oid::from_str(sha1).unwrap();
+    let head_oid = head.target().unwrap();
+    if head_oid == sha1_oid {
+        find_and_print_commit(repository, sha1_oid);
+        return true;
+    }
+
+    if repository.graph_descendant_of(head_oid, sha1_oid).unwrap() {
+        find_and_print_commit(repository, sha1_oid);
+        return true;
+    }
+    return false;
+}
+
+fn find_and_print_commit(repository: &Repository, sha1: Oid) {
+    let commit = repository.find_commit(sha1).unwrap();
+    let commit_datetime = chrono::DateTime::from_timestamp(commit.time().seconds(), 0).unwrap();
+
+    println!(
+        "Sha1 commit was {} ({}): {}",
+        commit.id(),
+        commit_datetime.format("%d/%m/%Y %H:%M"),
+        commit.message().unwrap().trim()
+    );
+}
+
+fn get_expected_sha1_from_crate(crates_io_path: &Path) -> Option<String> {
+    let cargo_vcs_info_path = crates_io_path.join(".cargo_vcs_info.json");
+    if !cargo_vcs_info_path.is_file() {
+        return None;
+    }
+
+    let mut vcs_info_json = File::open(cargo_vcs_info_path.to_path_buf()).unwrap();
+    let mut vcs_info_json_content = String::new();
+
+    vcs_info_json
+        .read_to_string(&mut vcs_info_json_content)
+        .unwrap();
+    let config: CargoVcsInfoJson = serde_json::from_str(vcs_info_json_content.as_str()).unwrap();
+
+    return Some(config.git.sha1.to_string());
 }
 
 fn get_repository_and_subpath_from_repository_url(crates_io_path: &Path) -> (String, String) {
-    let raw_repository_url = get_repository_url_from_cargo_toml(crates_io_path, );
+    let raw_repository_url = get_repository_url_from_cargo_toml(crates_io_path);
     let url_parsed = Url::parse(raw_repository_url.as_str()).unwrap();
 
     if url_parsed.host_str().unwrap() == "github.com" {
-        let paths: Vec<String> = url_parsed.path().split("/").map(|s| s.to_string()).collect();
+        let paths: Vec<String> = url_parsed
+            .path()
+            .split("/")
+            .map(|s| s.to_string())
+            .collect();
         return (
             format!(
                 "{}://{}/{}/{}.git",
@@ -66,8 +180,8 @@ fn get_repository_and_subpath_from_repository_url(crates_io_path: &Path) -> (Str
                 paths[5..].join("/")
             } else {
                 "".to_string()
-            }
-        )
+            },
+        );
     }
 
     (
@@ -77,16 +191,12 @@ fn get_repository_and_subpath_from_repository_url(crates_io_path: &Path) -> (Str
             url_parsed.host_str().unwrap(),
             url_parsed.path(),
         ),
-        "".to_string()
+        "".to_string(),
     )
 }
 
 fn get_repository_url_from_cargo_toml(crates_io_path: &Path) -> String {
-    let mut cargo_toml = File::open(
-        crates_io_path
-            .join("Cargo.toml")
-            .to_path_buf()
-    ).unwrap();
+    let mut cargo_toml = File::open(crates_io_path.join("Cargo.toml").to_path_buf()).unwrap();
     let mut cargo_toml_content = String::new();
 
     cargo_toml.read_to_string(&mut cargo_toml_content).unwrap();
@@ -96,10 +206,24 @@ fn get_repository_url_from_cargo_toml(crates_io_path: &Path) -> String {
 }
 
 fn download_crate(name: &str, version: &str, destination: &Path) {
-    println!("Downloading {}/{} to {}", name, version, destination.to_str().unwrap());
+    println!(
+        "Downloading {}/{} to {}",
+        name,
+        version,
+        destination.to_str().unwrap()
+    );
 
-    let url = format!("https://crates.io/api/v1/crates/{}/{}/download", name, version);
-    let archive: Vec<u8> = reqwest::blocking::get(url).unwrap().bytes().unwrap().to_vec();
+    let url = format!(
+        "https://crates.io/api/v1/crates/{}/{}/download",
+        name, version
+    );
+    let archive: Vec<u8> = reqwest::blocking::get(url)
+        .unwrap()
+        .bytes()
+        .unwrap()
+        .to_vec();
 
-    Archive::new(GzDecoder::new(Cursor::new(archive))).unpack(destination).unwrap();
+    Archive::new(GzDecoder::new(Cursor::new(archive)))
+        .unpack(destination)
+        .unwrap();
 }
